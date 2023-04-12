9b68a9329be1e96d3ac43a3ac4353e8e4a311aff
Martin Hinshelwood nkdAgility.com <martin@nkdagility.com>
2023-04-12 10:15:38 +0100

    Update

--- src/_plugins/copy-my-files.rb
+++ src/_plugins/copy-my-files.rb
@@ -2,8 +2,11 @@
 require 'fileutils'
 
 module Jekyll
-  Jekyll::Hooks.register :site, :post_write do |post|
+  Jekyll::Hooks.register :site, :post_read do |site|
+    STDOUT.puts "Moving _faveicon files  <-- not yet working"
+    FileUtils.touch('_site/file.txt')
     Dir.glob('_favicon/*.*') do |file|
+      STDOUT.puts "..moving"
       sourcePath = file
       outputPath = File.join('_site', File.basename(file) )
       FileUtils.cp(sourcePath, outputPath)
